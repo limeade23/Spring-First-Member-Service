aaa9b3793c916a155f9b3b9101173257226e82e1
Limeade <limeade@Limeadeui-MacBookPro.local>
2023-05-08 00:38:02 +0900

    Feat : MemberService

---
+++ src/main/java/com/example/memberservice/service/MemberService.java
@@ -0,0 +1,39 @@
+package com.example.memberservice.service;
+
+import com.example.memberservice.domain.Member;
+import com.example.memberservice.repository.MemberRepository;
+import com.example.memberservice.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    /*
+     * 회원 가입
+     */
+    public Long join(Member member) {
+        checkDuplicatedMemberName(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    // 중복 이름 확인
+    private void checkDuplicatedMemberName(Member member) {
+        memberRepository.findByName(member.getName())
+            .ifPresent(m -> {
+                throw new IllegalStateException("already exist member name!");
+            });
+    }
+
+    // 전체 회원 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
