e49c7c732d4a3a2ae7e47323ec2b19f0659dedf7
Limeade <limeade@Limeadeui-MacBookPro.local>
2023-05-16 23:59:58 +0900

    DI

--- src/main/java/com/example/memberservice/repository/MemoryMemberRepository.java
+++ src/main/java/com/example/memberservice/repository/MemoryMemberRepository.java
@@ -1,9 +1,11 @@
 package com.example.memberservice.repository;
 
 import com.example.memberservice.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository { // 여기서 Option(Alt) + Enter 하면 불러와줌
 
     /*
--- src/main/java/com/example/memberservice/service/MemberService.java
+++ src/main/java/com/example/memberservice/service/MemberService.java
@@ -3,21 +3,23 @@ package com.example.memberservice.service;
 import com.example.memberservice.domain.Member;
 import com.example.memberservice.repository.MemberRepository;
 import com.example.memberservice.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
     // 테스트 케이스 생성 시 Command + Shift + T
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
 
-    /*
-     * 회원 가입
-     */
+    /** 회원 가입 **/
     public Long join(Member member) {
         checkDuplicatedMemberName(member);
 
